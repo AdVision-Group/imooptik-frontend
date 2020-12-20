dabea4ba0ffd3c1114b6e18d0a191a5048d15877
Darken141 <darken141@gmail.com>
2020-12-20 17:52:36 +0100

    create analytics section

--- src/sections/analytics/analytics.section.jsx
+++ src/sections/analytics/analytics.section.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import SectionNavbar from '../../components/section-navbar/section-navbar.component'
+import ScrollContainer from '../../components/scroll-container/scroll-container.component'
+
+import {
+    Header,
+    GridContainer
+} from './analytics.styles'
 
 const AnalyticsSection = () => {
+    const items = [
+        {
+            id: 1,
+            name: "Všetko"
+        },
+        {
+            id: 2,
+            name: "Deň"
+        },
+        {
+            id: 3,
+            name: "Týžden"
+        },
+        {
+            id: 4,
+            name: "Mesiac"
+        },
+        {
+            id: 5,
+            name: "Rok"
+        },
+    ]
+
+    const [activeIndex, setActiveIndex] = useState(2)
+
     return (
         <section>
-            <h1>Analytika</h1>
+            <Header>
+                <h1>Analytiká</h1>
+            </Header>
+
+            <SectionNavbar
+                items={items}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+            />
+
+            <ScrollContainer>
+                <GridContainer>
+                    <div className='first' />
+                    <div className='second' />
+                    <div className='third' />
+                    <div className='fourth' />
+                </GridContainer>
+
+            </ScrollContainer>
         </section>
     )
 }
---
+++ src/sections/analytics/analytics.styles.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import { paddingStyles } from '../../global.styles'
+
+
+export const Header = styled.div`
+    ${paddingStyles}
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+`
+
+export const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-template-areas: 'f f l' 's t l';
+
+    div {
+        height: 20rem;
+    }
+
+    .first {
+        background-color: #eee;
+        grid-area: f;
+    }
+    .second {
+        background-color: #ddd;
+        grid-area: s;
+    }
+    .third {
+        background-color: #ccc;
+        grid-area: t;
+    }
+    .fourth {
+        background-color: #bbb;
+        grid-area: l;
+        height: 100%;
+    }
+`
