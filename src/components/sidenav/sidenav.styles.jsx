3d83f1f32d63bacb1898ead7f453e1257c76971d
Darken141 <darken141@gmail.com>
2020-12-30 16:20:25 +0100

    add scrolling bar

--- src/components/sidenav/sidenav.styles.jsx
+++ src/components/sidenav/sidenav.styles.jsx
@@ -34,6 +34,29 @@ export const LogoContainer = styled.div`
 export const NavLinksContainer = styled.ul`
     list-style: none;
 
+    @media all and (max-height: 725px) {
+        overflow-y: scroll;
+
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background:  #3E3E3E;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: #888;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #555;
+        }
+    }
+
     li {
         position: relative;
         display: flex;
