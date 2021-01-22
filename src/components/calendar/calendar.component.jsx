80de827e9ffe986ab22a699a4fc8d3fc7cfcd9a4
Darken141 <Darken141@gmail.com>
2021-01-22 22:12:19 +0100

    add comments to calendar component

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -36,35 +36,52 @@ const Calendar = ({ calendar, setSelectedDate }) => {
 
     const [activeIndex, setActiveIndex] = useState(null)
 
+    // v prvom rade si zistim datumy ktore treba
     const date = new Date()
+    //rok v ktoorm sa nachadzame
     const year = date.getFullYear()
+    // mesiac aky ma kalendar pouzit
     const month = date.getMonth()
+    // zistime aky den je prveho v nami zvoleneho mesiaca
     let firstDayOfCurrentMonth = new Date(year, month).getDay()
+    // a celkovy pocet dni v mesiaci
     const daysInMonth = new Date(year, month + 1, 0).getDate()
 
+    // ak je prvy den neela tak jej zmenit hodnotu na 7 pretoze nula robi bordel 
     if (firstDayOfCurrentMonth === 0) {
         firstDayOfCurrentMonth = 7
     }
 
+    // arr je pole kazdeho block v kalendary
     let arr = []
-    let j = 1
+    // ocislovanie dni
+    let blockNumber = 1
+
     let bookedDays = []
 
+    // sluzi na premenu {obj} s bookingmai na [arr] pre jednoduchsi for loop
     if (calendar.booked) {
         bookedDays = Object.keys(calendar.booked)
     }
-    // const bookedDays = ["28/12/2020", "28/12/2020", "28/12/2020", "28/12/2020", "28/12/2020", "25/12/2020", "30/12/2020", "28/12/2020", "25/12/2020", "30/12/2020"]
 
+    // a tu sa nam deje cele magia.. 42 je konstanta maximalnych dni 
+    // ktore sa mozu v kalendari nachadzat aj s pradnimy blokmi na zaciatku
     for (let i = 0; i < 42; i++) {
+        // pushneme do [arr] block s nulovym dnom(prazdne policko v kalendari) 
+        // ak i este nedociahne na prvy den v kalendary (preto je nedela 7)
         if (i < firstDayOfCurrentMonth - 1) {
             arr.push({
                 day: 0
             })
+            // ak uz prazdne dni (z minuleho mesiaca) mame zasebou 
         } else if (i < daysInMonth + firstDayOfCurrentMonth - 1) {
+            // pushneme do [arr] data ktore nam treba, cislo dna, pripadne bookingy atd
             let obj = {}
-            let numberOfBookings = 1
+
+            // skontroluje ci v dany den je nejaky booking ak ano, 
+            //doplni informacie o tom do {obj}
             bookedDays.forEach((day, idx) => {
-                if (j === Number(day.split('/')[0])) {
+                if (blockNumber === Number(day.split('/')[0])) {
                     obj = {
                         bookDate: day,
                         booked: calendar.booked[day].length,
@@ -73,9 +90,10 @@ const Calendar = ({ calendar, setSelectedDate }) => {
                 }
             })
 
+
             arr.push({
                 ...obj,
-                day: j++
+                day: blockNumber++
             })
         }
     }
@@ -89,6 +107,8 @@ const Calendar = ({ calendar, setSelectedDate }) => {
             value: bookDate
         })
     }
+
+    console.log(arr)
 
     return (
         <Container>
