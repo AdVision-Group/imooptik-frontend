bdbdf051d67826e630b00559997c5f7da5466d97
Darken141 <Darken141@gmail.com>
2021-02-10 21:22:15 +0100

    Updates

---
+++ static/js/34.d6f9a990.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpimooptik=this.webpackJsonpimooptik||[]).push([[34],{121:function(t,i,n){"use strict";n.d(i,"a",(function(){return a})),n.d(i,"b",(function(){return o}));var e=n(0);function a(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 6 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M4 7V5H2v2H0l3 4 3-4H4z"}}]})(t)}function o(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 6 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M3 5L0 9h2v2h2V9h2L3 5z"}}]})(t)}}}]);
+//# sourceMappingURL=34.d6f9a990.chunk.js.map
