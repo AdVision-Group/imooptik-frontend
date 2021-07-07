3a4862d30ecf8d22409f0e05b5954fe5e0d11e29
Darken141 <darken141@gmail.com>
2021-07-07 11:41:07 +0200

    fix aside active tab bug

--- src/components/sidenav/sidenav.component.jsx
+++ src/components/sidenav/sidenav.component.jsx
@@ -1,77 +1,89 @@
-import React, { useState, useContext } from 'react'
-import { AuthContext } from '../../context/auth/auth.context'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from "react"
+import { AuthContext } from "../../context/auth/auth.context"
+import { Link, useLocation } from "react-router-dom"
 
-import logo from '../../images/logo.png'
+import logo from "../../images/logo.png"
 
-import { AiOutlineLogout } from 'react-icons/ai'
+import { AiOutlineLogout } from "react-icons/ai"
 
 import {
-    AsideContainer,
-    LogoContainer,
-    NavLinksContainer,
-    ToggleButton,
-    NavLinksButton,
-    ActiveBorder,
-    IconContainer
-} from './sidenav.styles'
+	AsideContainer,
+	LogoContainer,
+	NavLinksContainer,
+	ToggleButton,
+	NavLinksButton,
+	ActiveBorder,
+	IconContainer,
+} from "./sidenav.styles"
 
 const Sidenav = ({ routes, match }) => {
-    const { logOut, currentUser, logout } = useContext(AuthContext)
+	const location = useLocation()
+	const { currentUser, logout } = useContext(AuthContext)
 
-    const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false)
 
-    // const [activeLink, setActiveLink] = useState(0)
-    const [activeLink, setActiveLink] = useState(4)
+	// const [activeLink, setActiveLink] = useState(0)
+	const [activeLink, setActiveLink] = useState(4)
 
-    const handleClick = (path, idx) => {
-        if (isOpen) {
-            setIsOpen(false)
-        }
-        setActiveLink(idx)
-        // push(path)
-    }
+	const handleClick = (path, idx) => {
+		console.log(path)
+		console.log(location)
+		if (isOpen) {
+			setIsOpen(false)
+		}
+		setActiveLink(idx)
+		// push(path)
+	}
 
-    return (
-        <React.Fragment>
-
-            <ToggleButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
-                <div />
-                <div />
-                <div />
-            </ToggleButton>
-            <AsideContainer isOpen={isOpen}>
-                <LogoContainer isOpen={isOpen}>
-                    <img src={logo} alt='imooptik logo' />
-                    <p>Prihlásený</p>
-                    <p>{currentUser?.name}</p>
-                </LogoContainer>
-                <NavLinksContainer>
-                    {routes.map((route, idx) => (
-                        <Link key={idx} to={match.url + route.path} onClick={() => handleClick(match.url + route.path, idx)}>
-                            <li  >
-                                <NavLinksButton>
-                                    <IconContainer isActive={activeLink === idx}>
-                                        <route.icon />
-                                    </IconContainer> {route.name}
-                                </NavLinksButton>
-                                <ActiveBorder isActive={activeLink === idx} />
-                            </li>
-                        </Link>
-                    ))}
-                    <li onClick={() => logout(() => {})}>
-                        <NavLinksButton>
-                            <IconContainer>
-                                <AiOutlineLogout />
-                            </IconContainer> Odhlásiť sa
-                            <ActiveBorder />
-                        </NavLinksButton>
-                    </li>
-                </NavLinksContainer>
-                <div />
-            </AsideContainer>
-        </React.Fragment>
-    )
+	return (
+		<React.Fragment>
+			<ToggleButton onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
+				<div />
+				<div />
+				<div />
+			</ToggleButton>
+			<AsideContainer isOpen={isOpen}>
+				<LogoContainer isOpen={isOpen}>
+					<img src={logo} alt="imooptik logo" />
+					<p>Prihlásený</p>
+					<p>{currentUser?.name}</p>
+				</LogoContainer>
+				<NavLinksContainer>
+					{routes.map((route, idx) => (
+						<Link
+							key={idx}
+							to={match.url + route.path}
+							onClick={() => handleClick(match.url + route.path, idx)}
+						>
+							<li>
+								<NavLinksButton>
+									<IconContainer
+										isActive={location.pathname.includes(route.path)}
+									>
+										<route.icon />
+									</IconContainer>{" "}
+									{route.name}
+								</NavLinksButton>
+								<ActiveBorder
+									isActive={location.pathname.includes(route.path)}
+								/>
+							</li>
+						</Link>
+					))}
+					<li onClick={() => logout(() => {})}>
+						<NavLinksButton>
+							<IconContainer>
+								<AiOutlineLogout />
+							</IconContainer>{" "}
+							Odhlásiť sa
+							<ActiveBorder />
+						</NavLinksButton>
+					</li>
+				</NavLinksContainer>
+				<div />
+			</AsideContainer>
+		</React.Fragment>
+	)
 }
 
 export default Sidenav
